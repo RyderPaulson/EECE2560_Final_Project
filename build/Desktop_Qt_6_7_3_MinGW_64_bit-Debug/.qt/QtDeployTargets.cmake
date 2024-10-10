c1f78f172b7932599dfeed1d3ea2fae9a3ec8b5e
RwpRwpTwo <rwprwptwo@gmail.com>
2024-10-10 13:29:33 -0400

    Commit w/ files.

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_EECE2560_Final_Project_FILE C:/Users/Ryder/Projects/Classes/EECE2560_Final_Project/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/EECE2560_Final_Project.exe)
+set(__QT_DEPLOY_TARGET_EECE2560_Final_Project_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_EECE2560_Final_Project_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
