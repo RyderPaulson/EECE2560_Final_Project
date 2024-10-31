5cbe750246e96ea6f0b53dd954adc8d22022e676
RwpRwpTwo <rwprwptwo@gmail.com>
2024-10-31 12:44:31 -0400

    Added IO header and file for functions focused on input and output.

---
+++ io.cpp
@@ -0,0 +1,6 @@
+#include "io.h"
+
+void outputCSV(string title, string date, double price){
+    fstream fout;
+
+}
---
+++ io.h
@@ -0,0 +1,10 @@
+#include <fstream>
+#include <string>
+using namespace std;
+
+#ifndef EECE2560_FINAL_PROJECT_IO_H
+#define EECE2560_FINAL_PROJECT_IO_H
+
+void outputCSV(string title, string date, double price);
+
+#endif //EECE2560_FINAL_PROJECT_IO_H
