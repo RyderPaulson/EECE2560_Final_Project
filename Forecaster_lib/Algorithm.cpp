b94afdaa0de2a65e8406840c0b9e0f5c6cc42247
RwpRwpTwo <rwprwptwo@gmail.com>
2024-11-03 18:23:31 -0500

    Partially completed forecast algorithm.

--- Forecaster_lib/Algorithm.cpp
+++ Forecaster_lib/Algorithm.cpp
@@ -2,36 +2,133 @@
 #include "Market.h"
 using namespace std;
 
-// Entrypoint
-SharePrice forecastValue(Company company){
-    vector<SharePrice> price_history = company.getPriceHistory();
-    addTime_T(price_history);
-    normalizeXAxis(price_history);
-    double mean_difference = volatility(price_history);
-    vector<SharePrice> mean_line = meanLine(price_history);
-    double standard_deviation = standardDeviation(price_history);
-
+Regression::Regression() {
+    normalized = false;
+    calculatedMeanLine = false;
+    coeff = 0;
+    const_term = 0;
+    sum_y = 0;
+    sum_y_square = 0;
+    sum_x_square = 0;
+    sum_x = 0;
+    sum_xy = 0;
 }
 
-vector<SharePrice> meanLine(vector<SharePrice>) {
+Regression::Regression(vector<SharePrice>& vec) {
+    data = vec;
 
-}
+    normalizeXAxis();
+    standardInterval();
 
-void normalizeXAxis(vector<SharePrice>& vec) {
-    double norm_factor = vec[0].std_t;
-    for(int i = 0; i < vec.size() - 1; i++) {
-        vec[i].std_t -= norm_factor;
+    for(int i =0; i < vec.size(); i++) {
+        sum_x += data[i].std_t;
+        sum_y += data[i].price;
+        sum_xy += data[i].std_t * data[i].price;
+        sum_x_square += data[i].std_t * data[i].std_t;
+        sum_y_square += data[i].price * data[i].price;
     }
+
+    calculateCoefficient();
+    calculateConstantTerm();
+
+    for(int i = 0; i < data.size(); i++) {
+        mean_line.push_back(meanLineFofX(data[i].std_t));
+    }
+    calculatedMeanLine = true;
+
+    calcVolatility();
+    standardDeviation();
 }
 
-double volatility(vector<SharePrice>& vec) {
+void Regression::normalizeXAxis() {
+    double norm_factor = data[0].std_t;
+    for(int i = 0; i < data.size() - 1; i++) {
+        data[i].std_t -= norm_factor;
+    }
+    normalized = true;
+}
+
+double Regression::calcVolatility() {
     double numerator = 0;
-    for(int i = 0; i < vec.size() - 1; i++){
-        numerator += vec[i+1].price - vec[i].price;
+    for(int i = 0; i < data.size() - 1; i++){
+        numerator += data[i+1].price - data[i].price;
     }
-    return numerator/vec.size();
+    volatility = numerator/data.size();
+    return numerator/data.size();
 }
 
-double standardDeviation(vector<SharePrice> vec) {
+double Regression::calculateCoefficient() {
+    double N = data.size();
+    float numerator = (N * sum_xy - sum_x * sum_y);
+    double denominator = (N * sum_x_square - sum_x * sum_x);
+    coeff = numerator / denominator;
+    return coeff;
+}
 
+double Regression::calculateConstantTerm()
+{
+    double N = data.size();
+    double numerator = sum_y * sum_x_square - sum_x * sum_xy;
+    double denominator = N * sum_x_square - sum_x * sum_x;
+    const_term = numerator / denominator;
+    return const_term;
+}
+
+void Regression::forecastMeanLine(int n) {
+    vector<SharePrice> new_values;
+    for(int i = 0; i < n; i++) {
+        new_values.push_back(meanLineFofX(std_interval*(mean_line.size() + i)));
+    }
+    mean_line.insert(mean_line.end(), new_values.begin(), new_values.end());
+}
+
+void Regression::forecastFutureValues(int n) {
+    forecastMeanLine(n);
+
+    double deviation;
+    SharePrice sp;
+
+    sp.std_t = data.end()->std_t;
+    deviation = data[size].price - mean_line[size].price;
+    sp.price = deviation/std_deviation*volatility;
+    for(int i = 1; i < n; i++) {
+        sp.std_t = std_interval*(size+i);
+        deviation = data[i-1].price - mean_line[i-1].price;
+        sp.price = deviation/std_deviation*volatility;
+        forecasted_line.push_back(sp);
+    }
+
+}
+
+SharePrice Regression::meanLineFofX(time_t x) {
+    SharePrice sp;
+    sp.std_t = x;
+    sp.price = coeff * x + const_term;
+    return sp;
+}
+
+double Regression::standardDeviation() {
+    int n = data.size();
+
+    double mean_y = sum_y / n;
+
+    double sum_x_diff_squared = 0, sum_y_diff_squared = 0;
+    for (int i = 0; i < n; i ++) {
+        // sum_y_diff_squared += ( - mean_y) * (point.y - mean_y);
+    }
+    double variance_x = sum_x_diff_squared / (n - 1);
+    double variance_y = sum_y_diff_squared / (n - 1);
+
+    double x_std_dev = sqrt(variance_x);
+    double y_std_dev = sqrt(variance_y);
+}
+
+double Regression::standardInterval() {
+    double sum = 0;
+    if(!normalized) normalizeXAxis();
+    for(int i = 0; i < data.size(); i++) {
+        sum += data[i].std_t;
+    }
+    std_interval = sum/data.size();
+    return std_interval;
 }
--- Forecaster_lib/Algorithm.h
+++ Forecaster_lib/Algorithm.h
@@ -11,10 +11,40 @@ using namespace std;
 #ifndef EECE2560_FINAL_PROJECT_ALGORITHM_H
 #define EECE2560_FINAL_PROJECT_ALGORITHM_H
 
-SharePrice forecastValue(Company company); // Entrypoint for algorithm
-vector<SharePrice> meanLine(vector<SharePrice> vec);
-void normalizeXAxis(vector<SharePrice>& vec);
-double volatility(vector<SharePrice>& vec);
-double standardDeviation(vector<SharePrice>& vec);
+class Regression{
+private:
+    vector<SharePrice> data;
+    vector<SharePrice> modified_data;
+    bool normalized, calculatedMeanLine;
+    int initial_size, size;
+    double coeff;
+    double const_term;
+    double sum_xy;
+    double sum_x;
+    double sum_y;
+    double sum_x_square;
+    double sum_y_square;
+    double volatility;
+    double std_interval;
+    double std_deviation;
+
+public:
+    vector<SharePrice> mean_line;
+    vector<SharePrice> forecasted_line;
+    double slope, intercept;
+
+    Regression();
+    Regression(vector<SharePrice>& vec);
+    void normalizeXAxis();
+    double calcVolatility();
+    double calculateCoefficient();
+    double calculateConstantTerm();
+    void forecastMeanLine(int n);
+    void forecastFutureValues(int n);
+    SharePrice meanLineFofX(time_t x);
+    double standardDeviation();
+    double standardInterval();
+};
+
 
 #endif //EECE2560_FINAL_PROJECT_ALGORITHM_H
