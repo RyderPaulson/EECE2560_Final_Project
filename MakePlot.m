927b1e4ca156179a4f13952b253ad94843f8d255
RwpRwpTwo <rwprwptwo@gmail.com>
2024-11-04 14:16:44 -0500

    Implemented plotting. Currently in testing form needs minor changes outlined in comments.

--- MakePlot.m
+++ MakePlot.m
@@ -1,2 +1,22 @@
-dateString = "12/25/2023"; % example date in MM/DD/YYYY format
-dateTimeObj = datetime(dateString, 'InputFormat', 'MM/dd/yyyy');
+% Change to just output.csv in final version
+T = readtable("data\Example Output.csv");
+
+Ticker = T.Ticker(1);
+Dates = datetime(T.Date, 'InputFormat', 'MM/dd/yyyy');
+Price = T.Price;
+MeanLine = T.MeanLine;
+Forecasted = T.Forecasted;
+
+% Get rid of this in final version
+MeanLine = ([1:1:29]*1.75261+100.301).';
+
+figure
+plot(Dates, Price);
+hold on
+plot(Dates, MeanLine);
+plot(Dates, Forecasted);
+hold off
+title(Ticker);
+legend('Price', 'Mean','Forecasted','Location','southeast');
+xlabel('Date')
+ylabel('Price ($)')
