9cdb94b2dfde9f58df802459b6e330ede0b1f5d5
RwpRwpTwo <rwprwptwo@gmail.com>
2024-11-05 14:56:06 -0500

    Corrected first line output of outputCSV function.

--- Forecaster_lib/io.cpp
+++ Forecaster_lib/io.cpp
@@ -7,8 +7,13 @@ void outputCSV(Regression forecast){
     string date;
     fout.open("data/output.csv");
 
+    date = forecast.company->getPriceHistory()[0].t.print();
+    fout<<date;
+    fout<<","<<forecast.company->getPriceHistory()[0].price;
+    fout<<","<<forecast.mean_line[0].price<<",,"<<forecast.company->getTicker()<<endl;
+
     fout<<"Date,Price,MeanLine,Forecasted,Ticker"<<endl;
-    for(int i = 0; i < forecast.getSize(); i++) {
+    for(int i = 1; i < forecast.getSize(); i++) {
         date = forecast.company->getPriceHistory()[i].t.print();
         fout<<date;
         fout<<","<<forecast.company->getPriceHistory()[i].price;
@@ -20,7 +25,7 @@ void outputCSV(Regression forecast){
         }
         if(start_forecast) {
             // If started, print it
-            fout<<","<<forecast.forecasted_line[i - forecast_start].price;
+            fout<<","<<forecast.forecasted_line[i-forecast_start].price;
         }
         else {
             // Otherwise just print comma to make empty value
